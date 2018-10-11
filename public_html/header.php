437993e26d8889f0d2637befead1282fe329fefd
Roland Johansson <hello@rolandjohansson.se>
2018-10-11 14:05:27 +0200

    navbar remade

--- public_html/header.php
+++ public_html/header.php
@@ -9,44 +9,50 @@
   <link rel="stylesheet" href="css/font-awesome.min.css">
   <link rel="stylesheet" href="css/master.css">
   <script src="js/navbar.js" defer></script>
+  <script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
 </head>
 
 <body>
-  <nav id="navbar">
-    <div class="wrapper">
-      <ul class="nav-header">
+  <section class="navigation">
+  <div class="nav-container">
+    <div class="brand">
+      <a href="/rj.se/public_html/index.php">rolandjohansson.se</a>
+    </div>
+    <nav>
+      <div class="nav-mobile"><a id="nav-toggle" href="#!"><span></span></a></div>
+      <ul class="nav-list">
         <li>
-          <a href="/rj.se/public_html/">rolandjohansson.se</a>
-        </li>
-        <li>
-          <a href="" id="nav-toggle-button" onclick="event.preventDefault()">
-            <i class="fa fa-bars" aria-hidden="true"></i>
-          </a>
-        </li>
-      </ul>
-      <div class="relative-container">
-      <ul class="nav-links hidden">
-        <li>
-          <ul>
-            <li class="nav-link-category">
-              <a href=""><span class="align-middle"></span> Tutorials<i class="fa fa-fw fa-caret-down" aria-hidden="true"></i></a>
-              <ul class="nav-link-list">
-                <li class="nav-link"><a href="tutorials-html-css.php" onclick="event.stopPropagation();">HTML & CSS</a></li>
-                <li class="nav-link"><a href="#" onclick="event.stopPropagation();">JavaScript</a></li>
-              </ul>
+          <a href="#!">Portfolio</a>
+          <ul class="nav-dropdown">
+            <li>
+              <a href="#!">Web Development</a>
             </li>
-            <li class="nav-link-category">
-              <a href=""><span class="align-middle"></span> Portfolio<i class="fa fa-fw fa-caret-down" aria-hidden="true"></i></a>
-              <ul class="nav-link-list">
-                <li class="nav-link"><a href="portfolio.php" onclick="event.stopPropagation();">Graphic Design</a></li>
-                <li class="nav-link"><a href="portfolio.php" onclick="event.stopPropagation();">Other projects</a></li>
-              </ul>
-              <a href="contact.php" onclick="event.stopPropagation();"><span class="align-middle"></span> Contact</a>
+            <li>
+              <a href="#!">Graphic Design</a>
             </li>
           </ul>
         </li>
-        <!-- target="_blank" rel="noopener noreferrer" -->
+        <li>
+          <a href="#!">Tutorials</a>
+          <ul class="nav-dropdown">
+            <li>
+              <a href="/rj.se/public_html/tutorials-html-css.php">HTML & CSS</a>
+            </li>
+            <li>
+              <a href="#!">JavaScript</a>
+            </li>
+            <li>
+              <a href="#!">Graphic Design</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="/rj.se/public_html/contact.php">About</a>
+        </li>
       </ul>
-      </div> <!-- /.relative-container -->
-    </div> <!-- /.wrapper -->
-  </nav>
+    </nav>
+  </div>
+</section>
--- public_html/js/navbar.js
+++ public_html/js/navbar.js
@@ -1,42 +1,107 @@
-//Toggle menu dropdown
+(function($) { // Begin jQuery
+  $(function() { // DOM ready
+    // If a link has a dropdown, add sub menu toggle.
+    $('nav ul li a:not(:only-child)').click(function(e) {
+      $(this).siblings('.nav-dropdown').toggle();
+      // Close one dropdown when selecting another
+      $('.nav-dropdown').not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+    // Clicking away from dropdown will remove the dropdown class
+    $('html').click(function() {
+      $('.nav-dropdown').hide();
+    });
+    // Toggle open and close nav styles on click
+    $('#nav-toggle').click(function() {
+      $('nav ul').slideToggle();
+    });
+    // Hamburger to X toggle
+    $('#nav-toggle').on('click', function() {
+      this.classList.toggle('active');
+    });
+  }); // end DOM ready
+})(jQuery); // end jQuery
 
-const NAVLINKCAT = document.querySelectorAll('.nav-link-category');
-const NAVLINKLIST = document.querySelector('.nav-link-list');
+// ===== Fade on Scroll ===== //
 
-function navLinkListToggle(e, clickedElement) {
-  e.preventDefault();
-  clickedElement.children[1].classList.toggle('dropped');
+var path = window.location.pathname;
+var page = path.split('/').pop();
+
+if (page !== 'index.php') {
+  $('.navigation').css('background-color', 'rgba(43, 57, 61, 1)');
+} else {
+
+  /*
+  fade-on-scroll.js
+  Author: Roland Johansson
+  Created: October 2018
+  */
+
+  //Definiera vid hur många pixlars bredd på viewporten vi vill ha vår brytpunkt
+  var breakpoint = 960;
+
+  //Definera den initiala bredden på viewporten
+  var previousWidth = $(window).width();
+
+  selectAlphaMode(previousWidth);
+
+  $(window).resize(function() {
+    let width = $(window).width();
+
+    if ((width < breakpoint && previousWidth >= breakpoint) || (width >= breakpoint && previousWidth < breakpoint)) {
+      selectAlphaMode(width);
+    }
+
+    previousWidth = width;
+
+  });
+
+  // Sätt alpha till 1 eller adaptiv baserat på viewportens bredd
+  function selectAlphaMode(width) {
+
+    if (width < breakpoint) {
+      $(document).scroll().off();
+      setAlpha(1);
+    } else {
+      setAlpha(calcAlpha());
+      $(document).scroll(function() {
+        setAlpha(calcAlpha());
+      });
+    }
+
+  }
+
+  function setAlpha(alpha) {
+    $('.navigation, .nav-dropdown').css('background-color', 'rgba(43, 57, 61,' + alpha + ')');
+  }
+
+  function calcAlpha() {
+    //Definiera variabel för alfa-värdet
+    let alpha = 0;
+
+    //Definiera en variabel för hur långt det är till toppen av HTML-dokumentet
+    var scroll = $(document).scrollTop();
+
+    //Vid vilken pixel ska toningen starta
+    var fadeStart = 70;
+
+    //Vid vilken pixel ska toningen sluta
+    var fadeStop = 170;
+
+    //Hur långt är det mellan start och stop
+    var fadeLength = fadeStop - fadeStart;
+
+    //Vi har tre cases, mellan 0 och fadeStart, mellan fadeStart och fadeStop, samt från fadeStop till oändligheten (och vidare)
+    if (scroll < fadeStart) {
+      alpha = 0;
+    } else if (scroll < fadeStop) {
+      alpha = (scroll - fadeStart) / fadeLength;
+    } else {
+      alpha = 1;
+    }
+
+    //Returnera vad alpha nu har blivit efter vår uträkning
+    return alpha;
+  }
+
 }
-
-Array.from(NAVLINKCAT).forEach(function(element) {
-  element.addEventListener('click', function(e) {
-    navLinkListToggle(e, this);
-  }, false);
-});
-
-/* ------------------------ */
-
-const NAVTOGGLEBUTTON = document.querySelector('#nav-toggle-button');
-const NAVLINKS = document.querySelector('.nav-links');
-const NAVLINK = document.querySelector('.nav-link');
-
-function toggleHide() {
-  NAVLINKS.classList.toggle('hidden');
-}
-
-NAVTOGGLEBUTTON.addEventListener('click', toggleHide, false);
-NAVLINK.addEventListener('click', toggleHide, false);
-
-/* ------------------------ */
-
-// function hideAll() {
-//   let result = document.querySelector('nav').classList.contains('dropped');
-//   console.log(result);
-//   // if (result) {
-//   //   console.log('hello');
-//   //   document.querySelectorAll('.dropped').classList.remove('dropped');
-//   // }
-//   // document.querySelectorAll('.dropped').classList.remove('dropped');
-// }
-
-// NAVLINK.addEventListener('click', hideAll, false);
